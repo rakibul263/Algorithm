5326c8693719f0c37adad4a776e19b49208d69c8
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-02-17 21:15:55 +0600

    naive Dijkstra'

---
+++ Dijkstra Algorithm/Dijkstra_naive.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 100;
+vector<pair<int, int>> adj[N];
+int dis[N];
+void dijkstra(int src)
+{
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    dis[src] = 0;
+    while (!q.empty())
+    {
+        pair<int, int> parent = q.front();
+        q.pop();
+        int node = parent.first;
+        int cost = parent.second;
+        for (pair<int, int> child : adj[node])
+        {
+            int childNode = child.first;
+            int childCost = child.second;
+            if (cost + childCost < dis[childNode])
+            {
+                // path relax
+                dis[childNode] = cost + childCost;
+                q.push({childNode, dis[childNode]});
+            }
+        }
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+        adj[b].push_back({a, c});
+    }
+    // memset(dis, INT_MAX, sizeof(dis));
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dijkstra(0);
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> " << dis[i] << endl;
+    }
+    return 0;
+}
