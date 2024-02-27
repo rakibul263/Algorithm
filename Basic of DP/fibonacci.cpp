f00ea11646459c9f236fcadc091f46cfb39518de
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-02-27 12:26:27 +0600

    fibonacci dp

--- Basic of DP/fibonacci.cpp
+++ Basic of DP/fibonacci.cpp
@@ -1,17 +1,27 @@
 #include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
-int fac(int n)
+const ll N = 1e6 + 5;
+ll dp[N];
+ll fac(ll n)
 {
     if (n == 0 || n == 1)
     {
         return n;
     }
-    return fac(n - 1) + fac(n - 2);
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+    ll ans = fac(n - 1) + fac(n - 2);
+    dp[n] = ans;
+    return ans;
 }
 int main()
 {
-    int n;
+    ll n;
     cin >> n;
-    cout << fac(n) <<endl;
+    memset(dp, -1, sizeof(dp));
+    cout << fac(n) << endl;
     return 0;
 }
